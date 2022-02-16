5f057df1c8a44c4f5a6534fc52fcdee980be638b
Wolfgang Skopalik-Lippert <95080091+wskopalik@users.noreply.github.com>
2022-02-16 15:16:57 +0100

    Add files via upload

---
+++ Python/CLD10xy_Controllers/CLD1010 using PyVISA.py
@@ -0,0 +1,23 @@
+#Import the PyVISA library to Python.
+import pyvisa
+
+#Opens a resource manager and shows the available VISA devices.
+rm = pyvisa.ResourceManager()
+rm.list_resources()
+
+#Opens the connection to the device. The variable instr is the handle for the device.
+#The 'USB0...' number for a device can e.g. be found in the returned list of rm.list_resources(). 
+instr = rm.open_resource('USB0::0x1313::0x804F::M00295819::INSTR')
+
+#Returns the identification of the device.
+#The command instr.query can be used when you want to get data from the device.
+print("Used device:", instr.query("*IDN?"))
+
+#Sets the current setpoint and queries the new setting for confirmation.
+#The command instr.write can be used to write data to the device when you do not expect a response from the device.
+instr.write("source1:current:level:amplitude 0.065")
+print("Set LD current [A]:", instr.query("source1:current:level:amplitude?"))
+
+#These lines will close the resource manager and the handle to the device.
+instr.close()
+rm.close()
