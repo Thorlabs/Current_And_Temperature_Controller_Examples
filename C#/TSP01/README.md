2ec0569d309581bbe13fdd494a0aa5a083a6108e
Matthew Anzelmi <manzelmi@Thorlabs.com>
2022-04-18 16:29:11 -0400

    Added gitignore and Example for TSP

---
+++ C#/TSP01/README.md
@@ -0,0 +1,12 @@
+Before building and running this example. Please make sure you have downloaded the TSP control app from here: https://www.thorlabs.com/software_pages/viewsoftwarepage.cfm?code=TSP
+
+This example is designed ONLY for the TSP01 Rev B hardware. Your hardware version can be checked by inspecting the back of the TSP unit. Any Rev B hardware will be indicated above the serial number. 
+
+1) Set the project platform under the properties menu. This should be set to match the intended development platform e.g. x64 for deployment on 64-bit machines. 
+
+2) Add the TLTSPB library as a reference by right clicking the References section of the Solution Explorer. Navigate to the appropriate folder for your platform target: 
+32-bit: C:\Program Files (x86)\IVI Foundation\VISA\VisaCom\Primary Interop Assemblies
+64-bit: C:\Program Files\IVI Foundation\VISA\VisaCom64\Primary Interop Assemblies
+
+This solution is pre-built for 64-bit systems, so it may be needed to delete and re-add the reference to this dll. 
+
---
+++ C#/TSP01/TSP_Read_All_Available_Data/TSP_Read_All_Available_Data/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thorlabs.TLTSP01B_64.Interop;
+
+namespace TSP_Read_All_Available_Data
+{
+    class Program
+    {
+        private static TLTSPB tltspb = null;
+
+        static void Main(string[] args)
+        {
+            tltspb = new TLTSPB("4883_33018_M00506857", false, false);
+            printInternalTemp();
+            printHum();
+            tltspb.Dispose();
+        }
+
+        static void printInternalTemp()
+        {
+            double temp;
+            tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel1, 0, out temp);
+            Console.WriteLine("Temperature for Internal Channel: " + temp);
+        }
+
+        static void printExtTemp1()
+        {
+            double temp;
+            tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel2, 0, out temp);
+            Console.WriteLine("Temperature for Channel 2: " + temp);
+        }
+
+        static void printExtTemp2()
+        {
+            double temp;
+            tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel3, 0, out temp);
+            Console.WriteLine("Temperature for Channel 3: " + temp);
+        }
+
+        static void printHum()
+        {
+            double humid;
+            tltspb.getHumidityData(0, out humid);
+            Console.WriteLine("Humidity Value: " + humid);
+        }
+
+        static void setOffset(int offset)
+        {
+            tltspb.setTempSensOffset(TLTSPBConstants.TltspTemperChannel1, offset);
+            System.Threading.Thread.Sleep(500);
+
+        }
+
+        static void printOffset()
+        {
+            double temperatureValue;
+            tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel1, 0, out temperatureValue);
+            Console.WriteLine("Current Temp Offset: " + temperatureValue);
+        }
+
+        static void setThermExp(double r0, double t0, double beta)
+        {
+            tltspb.setThermistorExpParams(TLTSPBConstants.TltspTemperChannel2, r0, t0, beta);
+            System.Threading.Thread.Sleep(500);
+        }
+
+        static void printThermExp()
+        {
+            double r0;
+            double t0;
+            double beta;
+            tltspb.getThermistorExpParams(TLTSPBConstants.TltspTemperChannel2, 0, out r0, out t0, out beta);
+            Console.WriteLine("R_0 value: " + r0 + "/ T_0 value: " + t0 + "/ Beta value: " + beta);
+        }
+
+        static void printThermRes()
+        {
+            double res;
+            tltspb.getThermRes(TLTSPBConstants.TltspTemperChannel2, 0, out res);
+            Console.WriteLine("External Thermistor Resistance: " + res);
+
+        }
+
+
+    }
+}
