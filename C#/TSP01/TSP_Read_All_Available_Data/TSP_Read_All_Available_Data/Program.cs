b5ba21d849937fea23bc5f30b069181e9bd8e913
Matthew Anzelmi <manzelmi@Thorlabs.com>
2022-04-18 17:44:09 -0400

    Updated TSP example

--- C#/TSP01/TSP_Read_All_Available_Data/TSP_Read_All_Available_Data/Program.cs
+++ C#/TSP01/TSP_Read_All_Available_Data/TSP_Read_All_Available_Data/Program.cs
@@ -13,77 +13,62 @@ namespace TSP_Read_All_Available_Data
 
         static void Main(string[] args)
         {
-            tltspb = new TLTSPB("4883_33018_M00506857", false, false);
-            printInternalTemp();
-            printHum();
-            tltspb.Dispose();
-        }
+            //Initialize a empty TSP to allow for resource finding
+            tltspb = new TLTSPB(new IntPtr(0));
+            uint numDevices = 0;
+            tltspb.findRsrc(out numDevices);
+            if (numDevices == 0)
+            {
+                //Close if no devices are found
+                Console.WriteLine("No Devices Found");
+                return;
+            }
 
-        static void printInternalTemp()
-        {
+            // Get the resource name of the first available device
+            StringBuilder resourceName = new StringBuilder(256);
+            tltspb.getRsrcName(0, resourceName);
+            tltspb.Dispose(); //Close the reference to the empty TSP
+
+            //reinitialize with the found resource string. The 2 booleans are for ID Query and reset. These can both be false
+            tltspb = new TLTSPB(resourceName.ToString(), false, false);
+
+            //Measurements
+            //Device attributes for all "get" commands shoud be 0. 
+            //The attribute represents the current, min, or max value and would only need to be used when setting parmeters
+
+            //Measure the humidity
+            double humidity;
+            tltspb.getHumidityData(0, out humidity);
+            Console.WriteLine("Humidity Value: " + humidity);
+
+            //Measure the temperate using the internal sensor
             double temp;
             tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel1, 0, out temp);
             Console.WriteLine("Temperature for Internal Channel: " + temp);
+
+            //Attempt to measure data from the external probes. If none are connected an exception will be thrown
+            try
+            {
+                tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel2, 0, out temp);
+                Console.WriteLine("Temperature for External Probe 1: " + temp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sensor 1 is not connected");
+            }
+
+            try
+            {
+                tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel3, 0, out temp);
+                Console.WriteLine("Temperature for External Probe 2: " + temp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sensor 2 is not connected");
+            }
+            //Disconnect
+            tltspb.Dispose();
+            Console.ReadKey();
         }
-
-        static void printExtTemp1()
-        {
-            double temp;
-            tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel2, 0, out temp);
-            Console.WriteLine("Temperature for Channel 2: " + temp);
-        }
-
-        static void printExtTemp2()
-        {
-            double temp;
-            tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel3, 0, out temp);
-            Console.WriteLine("Temperature for Channel 3: " + temp);
-        }
-
-        static void printHum()
-        {
-            double humid;
-            tltspb.getHumidityData(0, out humid);
-            Console.WriteLine("Humidity Value: " + humid);
-        }
-
-        static void setOffset(int offset)
-        {
-            tltspb.setTempSensOffset(TLTSPBConstants.TltspTemperChannel1, offset);
-            System.Threading.Thread.Sleep(500);
-
-        }
-
-        static void printOffset()
-        {
-            double temperatureValue;
-            tltspb.getTemperatureData(TLTSPBConstants.TltspTemperChannel1, 0, out temperatureValue);
-            Console.WriteLine("Current Temp Offset: " + temperatureValue);
-        }
-
-        static void setThermExp(double r0, double t0, double beta)
-        {
-            tltspb.setThermistorExpParams(TLTSPBConstants.TltspTemperChannel2, r0, t0, beta);
-            System.Threading.Thread.Sleep(500);
-        }
-
-        static void printThermExp()
-        {
-            double r0;
-            double t0;
-            double beta;
-            tltspb.getThermistorExpParams(TLTSPBConstants.TltspTemperChannel2, 0, out r0, out t0, out beta);
-            Console.WriteLine("R_0 value: " + r0 + "/ T_0 value: " + t0 + "/ Beta value: " + beta);
-        }
-
-        static void printThermRes()
-        {
-            double res;
-            tltspb.getThermRes(TLTSPBConstants.TltspTemperChannel2, 0, out res);
-            Console.WriteLine("External Thermistor Resistance: " + res);
-
-        }
-
-
     }
 }
