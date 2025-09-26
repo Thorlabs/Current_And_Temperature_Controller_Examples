83d7d77cc5b45c4164a5e65f89384ea079681e8a
gboedecker <125995107+gboedecker@users.noreply.github.com>
2025-09-26 14:51:10 +0200

    CLD ctypes example added

---
+++ Python/CLD10xy Laser Diode Controllers/CLD1010_ctypes.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Title:CLD1010_ctypes. 
+# Created Date: 2025 - 09 - 24
+# Last modified date: 2025 - 09 - 24
+# Python Version Used: Python 3.13
+# Thorlabs Driver Version: 3.1.0
+# Notes: This example demonstrates how to control a Thorlabs CLD laser diode controller in Python using CTypes library. 
+
+import os
+import time
+from ctypes import *
+
+os.chdir(r"C:\Program Files\IVI Foundation\VISA\Win64\Bin")
+lib = cdll.LoadLibrary("TL4000_64.dll")
+
+itc_handle=c_int(0)
+
+#documentation: C:\Program Files\IVI Foundation\VISA\Win64\TL4000\manual
+
+#Start Scan- Resource name will need to be adjusted
+#windows device manager -> NI-VISA USB Device -> Properties -> Details -> Device Instance ID
+res=lib.TL4000_init(b"USB0::0x1313::0x804F::M00413716::INSTR",1,1,byref(itc_handle))
+if res==0:print("Device connected")
+
+# Temperature Setpoint
+lib.TL4000_setTempSetpoint(itc_handle,c_double(26.0))
+tempset=c_double()
+lib.TL4000_getTempSetpoint(itc_handle,0,byref(tempset))
+print("Temperature setpoint ",tempset.value)
+
+#Switch on TEC output
+lib.TL4000_switchTecOutput(itc_handle,1)
+
+time.sleep(2)
+
+#Measure temperature
+temperature=c_double(0)
+lib.TL4000_measTemp(itc_handle,byref(temperature))
+print("Measured temperature: ",temperature.value)
+
+#Laserdiode settings
+lib.TL4000_setLdOperatingMode(itc_handle,0)#0: constant current mode, 1: constant power mode
+
+lib.TL4000_setLdCurrSetpoint(itc_handle,c_double(0.04))# set current to 0.04 A
+
+time.sleep(1)
+
+current=c_double(0)
+lib.TL4000_getLdCurrSetpoint(itc_handle,0,byref(current))
+print("LD current is set to: ", current.value)
+
+#Close connection
+lib.TL4000_close(itc_handle)
--- Python/CLD10xy Laser Diode Controllers/README.md
+++ Python/CLD10xy Laser Diode Controllers/README.md
@@ -2,7 +2,8 @@
 
 ## CLD Compact Laser Diode Controller Open and Control
 
-This sample code shows how you can control a Thorlabs CLD laser diode controller in Python. It uses the PyVISA library to send SCPI commands to the controller and make or read settings on it.
+The PyVISA sample code shows how you can control a Thorlabs CLD laser diode controller in Python. It uses the PyVISA library to send SCPI commands to the controller and make or read settings on it.
+The ctypes sample uses the C library TL4000_64.dll that can be installed from the Thorlabs website.
 
 Please note that the resource name (“USB0::0x1313::…”) in the sample code needs to be adjusted to the used controller type and unit.
 
